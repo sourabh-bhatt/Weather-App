cd77babf4636e53e2ec49d7c06c5b01eef053a05
Sourabh Bhatt <sourabhbhatt825@gmail.com>
2023-07-06 17:23:17 +0530

    created variable stateName, cityName, humidity, windSpeed, temrature, logoImage, weatherStatus to  store their respective  element

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ let data;
 // creating variables for fields
 const inputBox = document.getElementById("inputBoxx");
 const countryName = document.getElementById("countryName");
+const stateName = document.getElementById("stateName");
+const cityName = document.getElementById("cityName");
+const humidity = document.getElementById("humidity");
+const windSpeed = document.getElementById("windSpeed");
+const temprature = document.getElementById("temprature");
+const logoImage = document.getElementById("logoImage");
+const weatherStatus = document.getElementById("weatherStatus");
