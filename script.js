789e4f6c29cf9bc5f0652d7fbd5f385d33631d33
Sourabh Bhatt <sourabhbhatt825@gmail.com>
2023-07-06 18:02:18 +0530

    getting the data from api and changing the inner html

--- script.js
+++ script.js
@@ -12,6 +12,30 @@ const logoImage = document.getElementById("logoImage");
 const weatherStatus = document.getElementById("weatherStatus");
 
 // preventing the defaut input
-const getData = (event) => {
+const getData = async (event) => {
   event.preventDefault();
+  if (!inputBox.value) {
+    alert("Please Enter The City Name: ");
+    return;
+  }
+  const city = inputBox.value;
+
+  //   Fetching the details from weather api
+
+  const fetchData = await fetch(
+    `https://api.weatherapi.com/v1/forecast.json?key=7beb31318f1145eb870111309230607&q=${city}`
+  );
+
+  const orgData = await fetchData.json();
+  data = orgData;
+  console.log(data);
+
+  countryName.innerHTML = data.location.country;
+  stateName.innerHTML = data.location.region;
+  cityName.innerHTML = data.location.name;
+  humidity.innerHTML = data.current.humidity;
+  windSpeed.innerHTML = data.current.wind_kph;
+  temprature.innerHTML = data.current.temp_c;
+  logoImage.src = data.current.condition.icon;
+  weatherStatus.innerHTML = data.current.condition.text;
 };
